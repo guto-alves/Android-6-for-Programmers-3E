fadeabaabd1cb4f7c988b2fa666eecdc0a527fc9
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-12-27 20:07:12 -0300

    Add app Flag Quiz

---
+++ FlagQuiz/app/src/main/java/com/gutotech/flagquiz/SettingsActivityFragment.java
@@ -0,0 +1,15 @@
+package com.gutotech.flagquiz;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.annotation.Nullable;
+
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
